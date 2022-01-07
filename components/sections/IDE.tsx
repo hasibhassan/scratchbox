993430fc3a566e4647abe534ff31206f1e2f1182
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-07 09:55:46 -0600

    refactor: add prettier to ide

--- components/sections/IDE.tsx
+++ components/sections/IDE.tsx
@@ -7,11 +7,13 @@ import '@codesandbox/sandpack-react/dist/index.css'
 import Editor from '@ui/Editor'
 import Preview from '@ui/Preview'
 import reactTSCustomSetup from '@utils/reactTSCustomSetup'
+import Prettier from '@ui/Prettier'
 
 export default function IDE() {
   return (
     <SandpackProvider template="react-ts" customSetup={reactTSCustomSetup}>
       <SandpackThemeProvider theme={'github-light'}>
+        <Prettier />
         <Split className="split" minSize={200}>
           <Editor />
           <Preview />
--- components/ui/Prettier.tsx
+++ components/ui/Prettier.tsx
@@ -1,1 +1,41 @@
-export default function Prettier() {}
+import { useCallback, useEffect, useState } from 'react'
+import { useActiveCode, useSandpack } from '@codesandbox/sandpack-react'
+import prettier from 'prettier'
+import parserBabel from 'prettier/parser-babel'
+
+export default function Prettier() {
+  const [prettierCode, setPrettierCode] = useState('')
+  const { sandpack } = useSandpack()
+  const activeCode = useActiveCode()
+
+  const runPrettier = useCallback(() => {
+    if (activeCode.code) {
+      try {
+        /**
+         * I would recomend to run this process in a Worker
+         */
+        const formattedCode = prettier.format(activeCode.code, {
+          parser: 'babel',
+          plugins: [parserBabel],
+        })
+
+        setPrettierCode(formattedCode)
+      } catch {}
+    }
+  }, [activeCode.code])
+
+  useEffect(() => {
+    const debounce = setTimeout(runPrettier, 1000)
+    return () => {
+      clearInterval(debounce)
+    }
+  }, [runPrettier])
+
+  useEffect(() => {
+    if (prettierCode) {
+      sandpack.updateFile(sandpack.activePath, prettierCode)
+    }
+  }, [prettierCode])
+
+  return null
+}
