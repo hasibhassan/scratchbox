cfccf14dddc81c9780bfa8319f16d5a97ad6a239
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-07 08:55:07 -0600

    refactor: add custom editor setup

--- components/sections/IDE.tsx
+++ components/sections/IDE.tsx
@@ -6,10 +6,11 @@ import {
 import '@codesandbox/sandpack-react/dist/index.css'
 import Editor from '@ui/Editor'
 import Preview from '@ui/Preview'
+import reactTSCustomSetup from '@utils/reactTSCustomSetup'
 
 export default function IDE() {
   return (
-    <SandpackProvider>
+    <SandpackProvider template="react-ts" customSetup={reactTSCustomSetup}>
       <SandpackThemeProvider theme={'github-light'}>
         <Split className="split" minSize={200}>
           <Editor />
--- components/ui/Editor.tsx
+++ components/ui/Editor.tsx
@@ -1,5 +1,5 @@
 import { SandpackCodeEditor } from '@codesandbox/sandpack-react'
 
 export default function Editor() {
-  return <SandpackCodeEditor customStyle={{ height: '95vh' }} />
+  return <SandpackCodeEditor showTabs customStyle={{ height: '95vh' }} />
 }
