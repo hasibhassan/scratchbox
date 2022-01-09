143737214501b26fd6959a8de2541abde29e13de
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-09 16:22:52 -0600

    refactor: update ide

--- components/sections/IDE.tsx
+++ components/sections/IDE.tsx
@@ -1,24 +1,18 @@
 import Split from 'react-split'
-import {
-  SandpackProvider,
-  SandpackThemeProvider,
-} from '@codesandbox/sandpack-react'
+import { SandpackThemeProvider } from '@codesandbox/sandpack-react'
 import '@codesandbox/sandpack-react/dist/index.css'
 import Editor from '@ui/Editor'
 import Preview from '@ui/Preview'
-import reactTSCustomSetup from '@utils/reactTSCustomSetup'
 import Prettier from '@ui/Prettier'
 
 export default function IDE() {
   return (
-    <SandpackProvider template="react-ts" customSetup={reactTSCustomSetup}>
-      <SandpackThemeProvider theme={'github-light'}>
-        <Prettier />
-        <Split className="split" minSize={200}>
-          <Editor />
-          <Preview />
-        </Split>
-      </SandpackThemeProvider>
-    </SandpackProvider>
+    <SandpackThemeProvider theme={'github-light'}>
+      <Prettier />
+      <Split className="split" minSize={200}>
+        <Editor />
+        <Preview />
+      </Split>
+    </SandpackThemeProvider>
   )
 }
