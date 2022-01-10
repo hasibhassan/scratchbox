feb4ff9440262e02868c42d006779fa3ab6e863a
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-10 01:31:14 -0600

    refactor: update logo

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,3 +1,7 @@
 export default function Logo() {
-  return <img className="h-8 w-8" src="" alt="Scratchbox" />
+  return (
+    <a href="/">
+      <img className="h-8 w-8" src="logo.svg" alt="Scratchbox" />
+    </a>
+  )
 }
