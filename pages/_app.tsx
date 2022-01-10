347b72c4bf9eb4de74fbb6a73daa7ff77d7a31e0
Hasib Hassan <67892792+hasibhassan@users.noreply.github.com>
2022-01-10 12:21:43 -0600

    fix: update root app component imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import config from '@src/aws-exports'
+import config from '../src/aws-exports'
 import Amplify from 'aws-amplify'
 import { AmplifyProvider } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
